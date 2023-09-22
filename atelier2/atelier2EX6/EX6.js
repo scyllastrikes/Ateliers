2a2dd0948cd42213f316ca8f20783173fb98810a
scyllastrikes <ahmed.hamda.ah@gmail.com>
2023-09-22 12:29:15 +0100

    Final commit

---
+++ atelier2/atelier2EX6/EX6.js
@@ -0,0 +1,5 @@
+let paragrapheId=document.getElementById("paragraphe-id")
+let buttonId=document.getElementById("button-id")
+buttonId.addEventListener("click", ()=>{
+    paragrapheId.textContent="Le texte du paragraphe d'id paragraphe-id a été modifié"
+})
