69bc5c11dae2e364a6ae8b9b171660b35b5cf0fe
Mr. steal your set#8959 <125122145+scyllastrikes@users.noreply.github.com>
2023-09-27 14:41:22 +0100

    Update EX6.js

--- atelier2/atelier2EX6/EX6.js
+++ atelier2/atelier2EX6/EX6.js
@@ -1,5 +1,4 @@
 let paragrapheId=document.getElementById("paragraphe-id")
-let buttonId=document.getElementById("button-id")
-buttonId.addEventListener("click", ()=>{
-    paragrapheId.textContent="Le texte du paragraphe d'id paragraphe-id a été modifié"
-})
+function changer(){
+    paragrapheId.innerHTML="Le texte du paragraphe d'id paragraphe-id a été modifié"
+}
